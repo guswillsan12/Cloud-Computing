34c9ef6224cc71dc7e466dffdba26ec86d1c86e8
Gustavo Sanchez <guswillsan@gmail.com>
2023-02-14 23:06:06 -0500

    Subiendo archivo nuevo

---
+++ Ejercicio_1.py
@@ -0,0 +1,20 @@
+#Decoración: Nombre del Algoritmo
+print("-------------------------------------------------------")
+print("Ejercicio4: PUNTAJE TOTAL DE PARTIDOS.")
+print("-------------------------------------------------------")
+#Entradas
+print("Ingrese número de partidos ganados")
+PG = int( input())
+print("Ingrese número de partidos empatados")
+PE = int( input())
+print("Ingrese número de partidos perdidos")
+PP = int( input())
+#Proceso
+PPG = PG*3
+PPE = PE*1
+PPP = PP*0
+PF = PPG + PPE + PPP
+#Salida
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print("Puntaje Final: ", PF)
